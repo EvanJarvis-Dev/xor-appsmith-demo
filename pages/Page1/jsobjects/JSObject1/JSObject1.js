8b8225a51830e93bde27f7860d7aba4cbcfd76ad
Evan Jarvis <evan.xors@gmail.com>
2026-07-01 14:22:15 +0000

    changed everything to refer to stations

--- pages/Page1/jsobjects/JSObject1/JSObject1.js
+++
@@ -1,9 +0,0 @@
-export default {
-    playAudio(audioId) {
-
-        const url =
-            `https://livenviro-staging.net/api/audio?audioId=${audioId}`;
-
-        window.open(url, "_blank");
-    }
-}
