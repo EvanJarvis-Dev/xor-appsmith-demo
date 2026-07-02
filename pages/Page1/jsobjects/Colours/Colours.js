eccde0f81fa52c38d24a9e55a19eade443904d02
Evan Jarvis <evan.xors@gmail.com>
2026-07-02 09:46:09 +0000

    slightly adjusted positions

---
+++ pages/Page1/jsobjects/Colours/Colours.js
@@ -0,0 +1,19 @@
+export default {
+  colours: {
+    // Backgrounds
+    background: "#FDFDFE",
+    surface: "#E9ECF3",
+
+    // UI
+    border: "#DAD1D2",
+    muted: "#A2B1BA",
+
+    // Accent colours
+    success: "#4BC58F",
+    warning: "#F78555",
+
+    // Text
+    text: "#2F3A40",
+    textSecondary: "#A2B1BA"
+  }
+}
